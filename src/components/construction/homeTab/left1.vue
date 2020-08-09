6228760fdd48693b197f633e773e4822aaca2fd3
xiaokushuang <2314318218@qq.com>
2020-08-09 12:05:29 +0800

    左侧页面

--- src/components/construction/homeTab/left1.vue
+++ src/components/construction/homeTab/left1.vue
@@ -6,20 +6,81 @@
       </div>
       <!--工程-->
       <div class="clear object">
-        <div class="l objectItem" v-for="data in [1,2,3]">
+        <div class="l objectItem" v-for="data in objectLeftList1">
           <div>
-            <div class="objectItem-1">工程层数</div>
-            <div class="objectItem-2">+20/-3</div>
-            <div class="objectItem-3">层</div>
+            <div class="objectItem-1">{{data.name}}</div>
+            <div class="objectItem-2">{{data.num}}</div>
+            <div class="objectItem-3">{{data.unit}}</div>
           </div>
         </div>
       </div>
       <!--列表-->
+      <div class="objectList">
+        <div class="objectListItem clear" v-for="data in objectLeftList2">
+           <div class="l">{{data.name}}</div>
+           <div class="r">{{data.type}}</div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
     export default {
+      data() {
+        return {
+          objectLeftList1:[
+            {
+              name:'工程层数' ,
+              num:'+20/-3',
+              unit:'层'
+            },
+            {
+              name:'工程造价' ,
+              num:'36000',
+              unit:'万元'
+            },
+            {
+              name:'工程面积' ,
+              num:'50500',
+              unit:'平方米'
+            },
+          ],
+          objectLeftList2:[
+            {
+              name:'项目类型',
+              type:'工业建筑/工业建筑/厂房'
+            },
+            {
+              name:'工程类别',
+              type:'房建'
+            },
+            {
+              name:'项目类型',
+              type:'工业建筑/工业建筑/厂房'
+            },
+            {
+              name:'设计单位',
+              type:'设计单位设计单位设计单位设计单位'
+            },
+            {
+              name:'建设单位',
+              type:'通州建总集团'
+            },
+            {
+              name:'监理单位',
+              type:'监理单位'
+            },
+            {
+              name:'勘察单位',
+              type:'勘察单位'
+            },
+            {
+              name:'施工单位',
+              type:'施工单位'
+            },
+          ],
+        }
+      },
     }
 </script>
 
@@ -34,14 +95,30 @@
         width: 136px;
       margin-right: 15px;
       .objectItem-1{
+        width: 136px;
+        height: 30px;
+        text-align: center;
+        line-height: 30px;
+        font-size: 18px;
+        padding-left: 30px;
         background: url("./images/left/left-icon-2.png") 0 0 no-repeat;
       }
       .objectItem-2{
-
+        font-size: 42px;
+        color:#17C7FB;
+        margin-top: 5px;
       }
       .objectItem-3{
-
+        text-align: center;
       }
+    }
+  }
+  .objectList{
+    margin-top: 30px;
+    padding: 0 10px;
+    .objectListItem{
+      padding: 10px 0;
+      border-top: 1px solid #2D99FF;
     }
   }
 
--- src/components/construction/homeTab/left2.vue
+++ src/components/construction/homeTab/left2.vue
@@ -1,12 +1,76 @@
 <template>
-    <div></div>
+    <div class="left-2-class">
+      <!--小标题-->
+      <div class="border-title">
+        <img src="./images/left/left-icon-5.png" alt="">
+      </div>
+      <!--人数按钮-->
+      <div class="peopleNumBtn clear">
+        <div @click="changePeople(index)" style="margin-right: 20px;" class="l clear" :class="index==current?'hightLightLabel':'normalLabel'" v-for="(data,index) in peopleList">
+          <div class="l" style="margin-right: 20px;">
+            <div style="font-size: 24px;">{{data.num}}</div>
+            <div style="font-size: 18px;">{{data.name}}</div>
+          </div>
+          <div v-if="index=='0'" class="l" style="color:#25ACFF;margin-top: 32px;">|</div>
+        </div>
+      </div>
+      <!--echart-->
+      <div class="pie-echart">
+        <left2Echart></left2Echart>
+      </div>
+    </div>
 </template>
 
 <script>
+  import left2Echart from './left2-echart'
+  // import peiEchart from './left2-echart'
     export default {
+    components:{
+      left2Echart
+    },
+      data(){
+        return {
+          current:'0',
+          peopleList:[
+            {
+              num:'129',
+              name:'实时在场人数'
+            },
+            {
+              num:'137',
+              name:'今日考勤人数'
+            }
+          ],
+        }
+      },
+      methods:{
+        changePeople(val){
+          this.current = val
+        }
+      }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .left-2-class{
+    position: relative;
+    .border-title{
+      text-align: left;
+      margin-bottom: 20px;
+    }
+    .peopleNumBtn{
+      position: absolute;
+      right: -20px;
+      top: 3px;
+    }
+    .hightLightLabel{
+      cursor: pointer;
+      color:#CEF4FF;
+    }
+    .normalLabel{
+      cursor: pointer;
+      color:#6B6A7E
+    }
+  }
 
 </style>
